9f6001d508861e70098f57517a02d420c39ca86f
Chakib <mijikaihaikingu@tutanota.com>
2021-11-12 17:42:24 +0100

    read/notread checkbox added

--- script.js
+++ script.js
@@ -1,81 +1,48 @@
 let myLibrary = [];
-
 const bookModel = {
   init: function (title, author, year, nbPages, read) {
     this.title = title;
     this.author = author;
     this.year = `first release: ${year}`;
     this.nbPages = `${nbPages} pages`;
-    this.read = read ? "already read" : "not read yet";
+    this.read = read;
     return this;
   },
   // .info() doesn't work when using function expression... why???
   info: function () {
-    return `${this.title} by ${this.author}, first published in ${year}, ${this.nbPages} pages, ${this.read}.`;
+    return `${this.title} by ${this.author}, first published in ${year}, ${
+      this.nbPages
+    } pages, ${this.read ? "already read" : "not read yet"}.`;
   },
 };
-
-function addToLibrary(title, author, year, nbPages, read) {
-  let newBook = Object.create(bookModel).init(
-    title,
-    author,
-    year,
-    nbPages,
-    read
-  );
-  myLibrary.push(newBook);
-}
-
-addToLibrary("The Hobbit", "J.R.R. Tolkien", 1937, 295, false);
-addToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 1925, 110, true);
-addToLibrary("To Kill a Mocking Bird", "Harper Lee", 1960, 384, false);
-
-function displayLibrary() {
-  let collection = document.querySelector("#collection");
-  collection.innerHTML = "";
-  for (let i = 0; i < myLibrary.length; i++) {
-    let book = document.createElement("div");
-    collection.appendChild(book);
-    for (ownProp of Object.keys(myLibrary[i])) {
-      let elem = document.createElement("p");
-      elem.textContent = myLibrary[i][ownProp];
-      elem.classList.add(`${ownProp}`);
-      book.appendChild(elem);
-    }
-    let deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "-";
-    deleteBtn.classList.add("deleteBtn");
-    deleteBtn.addEventListener("click", () => {
-      myLibrary.splice(i, 1);
-      displayLibrary();
-    });
-    book.appendChild(deleteBtn);
-  }
-}
-
-let bookForm = document.querySelector("#bookForm");
+let collection = document.querySelector("#collection");
 let addBtn = document.querySelector("#addBook");
-
-addBtn.addEventListener("click", displayForm);
-
-function displayForm() {
-  bookForm.classList.remove("hiddenForm");
-}
-
+let bookForm = document.querySelector("#bookForm");
 let titleForm = document.querySelector("#titleForm");
 let authorForm = document.querySelector("#authorForm");
-let pagesForm = document.querySelector("#pagesForm");
 let yearForm = document.querySelector("#yearForm");
+let pagesForm = document.querySelector("#pagesForm");
 let readForm = document.querySelector("#readForm");
 let submitBtn = document.querySelector('button[type="submit"]');
-let cancelBtn = document.querySelector("#cancel");
 let errorMsg = document.querySelector("#error");
+let cancelBtn = document.querySelector("#cancel");
+
+addBtn.addEventListener("click", displayForm);
 submitBtn.addEventListener("click", submitBook);
 cancelBtn.addEventListener("click", cancelForm);
 window.addEventListener("keyup", (e) => {
   if (e.key === "Escape") bookForm.classList.add("hiddenForm");
 });
 
+addToLibrary("The Hobbit", "J.R.R. Tolkien", 1937, 295, false);
+addToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 1925, 110, true);
+addToLibrary("To Kill a Mocking Bird", "Harper Lee", 1960, 384, false);
+
+displayLibrary();
+
+function displayForm() {
+  bookForm.classList.remove("hiddenForm");
+}
 function submitBook(e) {
   e.preventDefault();
   if (
@@ -92,11 +59,11 @@ function submitBook(e) {
     pagesForm.value,
     readForm.checked
   );
-  /*   titleForm.value = "";
+  titleForm.value = "";
   authorForm.value = "";
   yearForm.value = "";
   pagesForm.value = "";
-  readForm.checked = false; */
+  readForm.checked = false;
   bookForm.classList.add("hiddenForm");
   displayLibrary();
 }
@@ -104,5 +71,47 @@ function cancelForm(e) {
   e.preventDefault();
   bookForm.classList.add("hiddenForm");
 }
-
-displayLibrary();
+function addToLibrary(title, author, year, nbPages, read) {
+  let newBook = Object.create(bookModel).init(
+    title,
+    author,
+    year,
+    nbPages,
+    read
+  );
+  myLibrary.push(newBook);
+}
+function displayLibrary() {
+  collection.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = document.createElement("div");
+    collection.appendChild(book);
+    //Object.keys also works as it does not take into account inherited keys,
+    //as opoosed to for...in;
+    //the only difference is that Object.keys only lists enumerable properties
+    //(no consequences here though)
+    for (ownProp of Object.getOwnPropertyNames(myLibrary[i])) {
+      let elem = document.createElement("p");
+      if (ownProp !== "read") elem.textContent = myLibrary[i][ownProp];
+      else {
+        elem.innerHTML = `already read? <input type='checkbox' ${
+          myLibrary[i][ownProp] ? "checked" : ""
+        }></input>`;
+        elem.firstElementChild.addEventListener(
+          "change",
+          () => (myLibrary[i].read = !myLibrary[i].read)
+        );
+      }
+      elem.classList.add(`${ownProp}`);
+      book.appendChild(elem);
+    }
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "-";
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.addEventListener("click", () => {
+      myLibrary.splice(i, 1);
+      displayLibrary();
+    });
+    book.appendChild(deleteBtn);
+  }
+}
