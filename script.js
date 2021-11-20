9f47cabcc5305d86b79b4497e1787e6adfffb949
Chakib <mijikaihaikingu@tutanota.com>
2021-11-20 15:28:58 +0100

    replaced constructor logic with class logic

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 let myLibrary = [];
-const bookModel = {
+
+class bookModel {
+  constructor(title, author, year, nbPages, read) {
+    this.title = title;
+    this.author = author;
+    this.year = `${year}`;
+    this.nbPages = `${nbPages}`;
+    this.read = read;
+  }
+
+  info() {
+    return `${this.title} by ${this.author}, first released in ${this.year}, ${
+      this.nbPages
+    } pages, ${this.read ? "already read" : "not read yet"}.`;
+  }
+}
+
+/* const bookModel = {
   init: function (title, author, year, nbPages, read) {
     this.title = title;
     this.author = author;
@@ -9,12 +26,13 @@ const bookModel = {
     return this;
   },
   // .info() doesn't work when using function expression... why???
+  // since ES6? we can simply write info() {...}
   info: function () {
     return `${this.title} by ${this.author}, ${this.year}, ${this.nbPages}, ${
       this.read ? "already read" : "not read yet"
     }.`;
   },
-};
+}; */
 let collection = document.querySelector("#collection");
 let addBtn = document.querySelector("#addBook");
 let bookForm = document.querySelector("#bookForm");
@@ -39,8 +57,11 @@ displayLibrary();
 
 function initLibrary() {
   if (localStorage.length && localStorage.getItem("library") !== "[]") {
-    myLibrary = JSON.parse(localStorage.getItem("library"));
-    myLibrary.forEach((book) => Object.setPrototypeOf(book, bookModel));
+    let tempLibrary = JSON.parse(localStorage.getItem("library"));
+    tempLibrary.forEach((book) =>
+      addToLibrary(book.title, book.author, book.year, book.nbPages, book.read)
+    );
+    /* myLibrary.forEach((book) => Object.setPrototypeOf(book, bookModel)); */
   } else {
     addToLibrary("The Hobbit", "J.R.R. Tolkien", 1937, 295, false);
     addToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 1925, 110, true);
@@ -48,13 +69,14 @@ function initLibrary() {
   }
 }
 function addToLibrary(title, author, year, nbPages, read) {
-  let newBook = Object.create(bookModel).init(
+  let newBook = new bookModel(title, author, year, nbPages, read);
+  /* let newBook = Object.create(bookModel).init(
     title,
     author,
     year,
     nbPages,
     read
-  );
+  ); */
   myLibrary.push(newBook);
   //the Storage.setItem the functionality seems to be limited to handle only string key/value pairs
   //that's why JSON.stringify is used as a workaround
@@ -102,8 +124,7 @@ function displayLibrary() {
     //(no consequences here though)
     for (ownProp of Object.getOwnPropertyNames(myLibrary[i])) {
       let elem = document.createElement("p");
-      if (ownProp !== "read") elem.textContent = myLibrary[i][ownProp];
-      else {
+      if (ownProp === "read") {
         elem.innerHTML = `${
           myLibrary[i][ownProp]
             ? '<button class="read">Read</button>'
@@ -111,7 +132,10 @@ function displayLibrary() {
         }`;
         elem.firstElementChild.setAttribute("data-index", i);
         elem.firstElementChild.addEventListener("click", toggleRead);
-      }
+      } else
+        elem.textContent = `${myLibrary[i][ownProp]}${
+          ownProp === "nbPages" ? " pages" : ""
+        }`;
       elem.classList.add(`${ownProp}`);
       book.appendChild(elem);
     }
